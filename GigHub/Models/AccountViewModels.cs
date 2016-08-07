c5387e3263ab33d0985ee5871108457a0407ac8f
rodrig67 <abel.arodriguez13@gmail.com>
2016-08-07 14:57:47 -0600

    Add name to artist.

--- GigHub/Models/AccountViewModels.cs
+++ GigHub/Models/AccountViewModels.cs
@@ -68,7 +68,7 @@ namespace GigHub.Models
         [EmailAddress]
         [Display(Name = "Email")]
         public string Email { get; set; }
-
+        
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
@@ -79,6 +79,10 @@ namespace GigHub.Models
         [Display(Name = "Confirm password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
     }
 
     public class ResetPasswordViewModel
